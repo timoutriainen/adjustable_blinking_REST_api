70b0eb316fe34c7605e4f13d06a8f22e8d976b90
Timo Utriainen <timo@example.com>
2018-10-18 17:20:33 +0300

    eslint fixes

--- routes/motor.js
+++ routes/motor.js
@@ -1,101 +1,100 @@
-
-const fs = require('fs');
 const Ajv = require('ajv');
+const Gpio = require('pigpio').Gpio;
+const debug = require('debug')('pwm');
 const postSpeedReqSchema = require('../validators/postSpeedRequest');
 const postRotationReqSchema = require('../validators/postRotationRequest');
 const MotorApi = require('../services/motor-api');
-const Gpio = require('pigpio').Gpio;
+
 const led = new Gpio(4, { mode: Gpio.OUTPUT });
 const left = new Gpio(17, { mode: Gpio.OUTPUT });
 const right = new Gpio(27, { mode: Gpio.OUTPUT });
-const debug = require('debug')('pwm');
 
-const pwm = MotorApi.create({led, right, left});
+const pwm = MotorApi.create({ led, right, left });
 
 const ajv = new Ajv({ allErrors: true });
 ajv.addSchema(postSpeedReqSchema, 'postSchema');
 
 function createErrorResponse(schemaErrors) {
-    const listOfErrors = schemaErrors.map(error => ({
-        path: error.dataPath,
-        message: error.message,
-    }));
+  const listOfErrors = schemaErrors.map(error => ({
+    path: error.dataPath,
+    message: error.message,
+  }));
 
-    return {
-        status: 'failed',
-        listOfErrors,
-    };
+  return {
+    status: 'failed',
+    listOfErrors,
+  };
 }
 
 const validatePostParams = (schema, req, res, next) => {
-    debug('validatePostParams()');
-    const status = ajv.validate(schema, req.params);
-    if (ajv.errors) {
-            debug(ajv.errors);
-    }
-    if (!status) {
-            debug('validatePostParams() - nok');
-            return res.send(createErrorResponse(ajv.errors));
-    }
-    debug('validatePostParams() - ok');
-    return next();
+  debug('validatePostParams()');
+  const status = ajv.validate(schema, req.params);
+  if (ajv.errors) {
+    debug(ajv.errors);
+  }
+  if (!status) {
+    debug('validatePostParams() - nok');
+    return res.send(createErrorResponse(ajv.errors));
+  }
+  debug('validatePostParams() - ok');
+  return next();
 };
 
 const validatePostDirectionParams = (req, res, next) => {
-    debug('validatePostDirectionParams()');
-    return validatePostParams(postRotationReqSchema, req, res, next);
+  debug('validatePostDirectionParams()');
+  return validatePostParams(postRotationReqSchema, req, res, next);
 };
 
 const validatePostSpeedParams = (req, res, next) => {
-    debug('validatePostSpeedParams()');
-    return validatePostParams(postSpeedReqSchema, req, res, next);
+  debug('validatePostSpeedParams()');
+  return validatePostParams(postSpeedReqSchema, req, res, next);
 };
 
 const handleGetRotationRequest = (req, res, next) => {
-    debug('handleGetRotationRequest()');
-    const rotation = pwm.getRotation();
-    debug(`rotation: ${rotation}`);
-    res.json({ rotation });
-    return next();
+  debug('handleGetRotationRequest()');
+  const rotation = pwm.getRotation();
+  debug(`rotation: ${rotation}`);
+  res.json({ rotation });
+  return next();
 };
 
 const handlePostRotationRequest = (req, res, next) => {
-    debug('handlePostRotationRequest()');
-    const { rotation } = req.params;
-    debug(`new rotation: ${rotation}`);
-    pwm.setRotation(rotation);
-    res.json({ rotation });
-    return next();
+  debug('handlePostRotationRequest()');
+  const { rotation } = req.params;
+  debug(`new rotation: ${rotation}`);
+  pwm.setRotation(rotation);
+  res.json({ rotation });
+  return next();
 };
 
 const handleGetSpeedRequest = (req, res, next) => {
-    debug('handleGetSpeedRequest()');
-    const speed = pwm.getSpeed();
-    debug(`speed is ${speed}`);
-    res.json({ speed });
-    return next();
+  debug('handleGetSpeedRequest()');
+  const speed = pwm.getSpeed();
+  debug(`speed is ${speed}`);
+  res.json({ speed });
+  return next();
 };
 
 const handlePostSpeedRequest = (req, res, next) => {
-    debug('handlePostSpeedRequest()');
-    const { speed } = req.params;
-    pwm.setSpeed(speed)
-    res.json({ speed });
-    return next();
+  debug('handlePostSpeedRequest()');
+  const { speed } = req.params;
+  pwm.setSpeed(speed)
+  res.json({ speed });
+  return next();
 };
 
 module.exports = (server) => {
-    server.post('/motor/rotation',
-        validatePostDirectionParams,
-        handlePostRotationRequest);
+  server.post('/motor/rotation',
+    validatePostDirectionParams,
+    handlePostRotationRequest);
 
-    server.get('/motor/rotation',
-        handleGetRotationRequest);
+  server.get('/motor/rotation',
+    handleGetRotationRequest);
 
-    server.post('/motor/speed',
-        validatePostSpeedParams,
-        handlePostSpeedRequest);
+  server.post('/motor/speed',
+    validatePostSpeedParams,
+    handlePostSpeedRequest);
 
-    server.get('/motor/speed',
-        handleGetSpeedRequest);
+  server.get('/motor/speed',
+    handleGetSpeedRequest);
 };
--- services/motor-api.js
+++ services/motor-api.js
@@ -9,68 +9,70 @@ let dependencies;
 let speed;
 let rotation;
 
+const setRotationToNone = function () {
+  debug('setRotationToNone()');
+  rotation = ROTATION_NONE;
+  dependencies.right.digitalWrite(0);
+  dependencies.left.digitalWrite(0);
+};
+
+const setRotationToLeft = function () {
+  debug('setRotationToLeft()');
+  rotation = ROTATION_LEFT;
+  dependencies.right.digitalWrite(0);
+  dependencies.left.digitalWrite(1);
+};
+
+const setRotationToRight = function () {
+  debug('setRotationToRight()');
+  rotation = ROTATION_RIGHT;
+  dependencies.left.digitalWrite(0);
+  dependencies.right.digitalWrite(1);
+};
+
 function MotorApi(motorIo) {
-    dependencies = motorIo;
-    debug('MotorApi: dependencies: %o', dependencies);
-    MotorApi.prototype.setSpeed(SPEED_ZERO);
-    setRotationToNone();
+  dependencies = motorIo;
+  debug('MotorApi: dependencies: %o', dependencies);
+  MotorApi.prototype.setSpeed(SPEED_ZERO);
+  setRotationToNone();
 }
 
-MotorApi.prototype.getSpeed = function() {
-    return speed;
-}
+MotorApi.prototype.getSpeed = function () {
+  return speed;
+};
 
-MotorApi.prototype.setSpeed = function(newSpeed) {
-    if (newSpeed > 255 || newSpeed < 0) {
-        debug(`invalid speed ${newSpeed}`);
-        return;
-    }
-    speed = newSpeed;
-    dependencies.led.pwmWrite(speed);
-}
+MotorApi.prototype.setSpeed = function (newSpeed) {
+  if (newSpeed > 255 || newSpeed < 0) {
+    debug(`invalid speed ${newSpeed}`);
+    return;
+  }
+  speed = newSpeed;
+  dependencies.led.pwmWrite(speed);
+};
 
-const setRotationToNone = function() {
-    debug('setRotationToNone()');
-    rotation = ROTATION_NONE;
-    dependencies.right.digitalWrite(0);
-    dependencies.left.digitalWrite(0);
-}
+MotorApi.prototype.setRotation = function (newRotation) {
+  debug(`setRotation(${newRotation})`);
+  if (newRotation !== ROTATION_LEFT
+    && newRotation !== ROTATION_RIGHT
+    && newRotation !== ROTATION_NONE) {
+    debug('invalid rotation value');
+    return;
+  }
+  if (newRotation === ROTATION_LEFT) {
+    return setRotationToLeft();
+  }
+  if (newRotation === ROTATION_RIGHT) {
+    return setRotationToRight();
+  }
+  setRotationToNone();
+};
 
-const setRotationToLeft = function() {
-    debug('setRotationToLeft()');
-    rotation = ROTATION_LEFT;
-    dependencies.right.digitalWrite(0);
-    dependencies.left.digitalWrite(1);
-}
-
-const setRotationToRight = function() {
-    debug('setRotationToRight()');
-    rotation = ROTATION_RIGHT;
-    dependencies.left.digitalWrite(0);
-    dependencies.right.digitalWrite(1);
-}
-
-MotorApi.prototype.setRotation = function(rotation) {
-    debug(`setRotation(${rotation})`);
-    if (rotation !== ROTATION_LEFT && rotation !== ROTATION_RIGHT & rotation !== ROTATION_NONE) {
-        debug('invalid rotation value');
-        return;
-    }
-    if (rotation === ROTATION_LEFT) {
-        return setRotationToLeft();
-    }
-    if (rotation === ROTATION_RIGHT) {
-        return setRotationToRight();
-    }
-    setRotationToNone();
-}
-
-MotorApi.prototype.getRotation = function() {
-    return rotation;
-}
+MotorApi.prototype.getRotation = function () {
+  return rotation;
+};
 
 function create(motorIo) {
-    return new MotorApi(motorIo);
+  return new MotorApi(motorIo);
 }
 
 module.exports = { create };
